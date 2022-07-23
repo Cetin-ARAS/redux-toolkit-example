849af178d5b5b44a080a442683c89ccb9af452f8
ÇETİN <cetinnnarasss@gmail.com>
2022-07-23 12:24:43 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import store from './app/store';
 import AppRouter from './router/AppRouter';
+import {Provider} from 'react-redux';
+
 
 function App() {
   return (
     <div>
+      <Provider store={store}>
       <AppRouter />
+      </Provider>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,11 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import {useNavigate} from "react-router-dom"
+import { useSelector } from 'react-redux';
 
 export default function Navbar() {
   const navigate = useNavigate()
-  const user = false
+  const user = useSelector((state) => state.auth.user);
 
   const handleLogout = () => {
       //! clear user data(logout olduğunda demek)

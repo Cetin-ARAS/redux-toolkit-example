9d977477e8265741ead1a50fa175d7ddacbe102a
ÇETİN <cetinnnarasss@gmail.com>
2022-07-23 11:54:45 +0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,31 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import {useNavigate} from "react-router-dom"
 
 export default function Navbar() {
+  const navigate = useNavigate()
+  const user = false
+
+  const handleLogout = () => {
+      //! clear user data(logout olduğunda demek)
+      navihate("login")
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}
+          onClick={()=> navigate('/')}>
+
             Clarusway News
           </Typography>
-
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+          {user ? (
+              <Button color="inherit" onClick={handleLogout}>Logout</Button> ):
+              (              
+              <Button color="inherit" onClick={() => navigate("/")}>
+                Login</Button> )}
+          </Toolbar>      
       </AppBar>
     </Box>
   );
